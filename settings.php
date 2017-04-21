cd27d050b99007af956014e87042ec1356cab2ec
Danny Tran <hoogleyboogley@minefieldmc.net>
2017-04-21 08:47:27 -0500

    Create settings.php

---
+++ settings.php
@@ -0,0 +1,15 @@
+<?php
+
+ 		#####################################
+$db =	#	INSERT DATABASE RETREVAL CODE	#
+ 		#####################################
+		
+if ($user_color = green) {
+	header "Location: green.php";
+} else {
+	if ($user_color = purple) {
+		header "Location: purple.php";
+	};
+};
+
+?>
